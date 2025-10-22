8cd4404507f2f0ec268d04d3509447cbf2a7fe23
FelipeMacariMagalhaes <felipe.magalhaes@alunos.sc.senac.br>
2025-10-22 14:58:24 -0300

    packjage

---
+++ A LUA/Assets/BOXOPHOBIC/Menu/MenuPrincipalManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    
+
+   public void jogar()
+   {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+   }
+
+   public void AbrirOpcoes()
+   {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+
+   }
+   public void FecharOpcoes()
+   {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+   }
+   public void SairJogo()
+   {
+    Debug.Log("Sair do Jogo");
+        Application.Quit();
+   }
+}
